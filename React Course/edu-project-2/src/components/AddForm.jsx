e0c0f912620dd845839ebf5061c701d1d36da140
Vladyslav_Pyrozhenko <vladyslav22pyrozhenko@gail.com>
2022-08-30 14:54:40 +0300

    add basic validation

--- React Course/edu-project-2/src/components/AddForm.jsx
+++ React Course/edu-project-2/src/components/AddForm.jsx
@@ -8,6 +8,8 @@ const AddForm = () => {
 
   const onSubmitHandler = (event) => {
     event.preventDefault();
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) return;
+    if (enteredAge < 1) return;
     const item = {
       username: enteredUsername,
       age: enteredAge,
